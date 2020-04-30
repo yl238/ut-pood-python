93cb4da40ffa397575ef0ff855a35d14f3c63409
Sue Liu <sue.liu@mudano.com>
2020-04-30 13:11:31 +0100

    Bottle number

--- pybottles/lib/bottles.py
+++ pybottles/lib/bottles.py
@@ -3,13 +3,16 @@ class Bottles:
         return self.verses(99, 0)
 
     def verses(self, upper, lower):
-        return '\n'.join([self.verse(i) for i in range(upper, lower-1, -1)])
+        return '\n'.join(map(self.verse, range(upper, lower-1, -1)))
 
     def verse(self, number):
-        return f"{self.amount(number).capitalize()} {self.container(number)} of beer on the wall, " \
-        f"{self.amount(number)} {self.container(number)} of beer.\n"\
-        f"{self.action(number)}, "\
-        f"{self.amount(self.successor(number))} {self.container(self.successor(number))} of beer on the wall.\n"
+        bottle_number = BottleNumber(number)
+        next_bottle_number = BottleNumber(bottle_number.successor)
+
+        return f"{bottle_number.quantity.capitalize()} {bottle_number.container} of beer on the wall, " \
+        f"{bottle_number.quantity} {bottle_number.container} of beer.\n"\
+        f"{bottle_number.action}, "\
+        f"{next_bottle_number.quantity} {next_bottle_number.container} of beer on the wall.\n"
 
     def amount(self, number):
         if number == 0:
@@ -40,6 +43,45 @@ class Bottles:
             return 99
         else:
             return number - 1
+
+class BottleNumber:
+    def __init__(self, number):
+        self.number = number
+        self.quantity = self.set_quantity()
+        self.container = self.set_container()
+        self.action = self.set_action()
+        self.successor = self.set_successor()
+
+    def set_quantity(self):
+        if self.number == 0:
+            return "no more"
+        else:
+            return str(self.number)
+
+    def set_container(self):
+        if self.number == 1:
+            return "bottle"
+        else:
+            return "bottles"
+
+    def set_action(self):
+        if self.number == 0:
+            return "Go to the store and buy some more"
+        else:
+            return f"Take {self.pronoun()} down and pass it around"
+    
+    def pronoun(self):
+        if self.number == 1:
+            return 'it'
+        else:
+            return 'one'
+    
+    def set_successor(self):
+        if self.number == 0:
+            return 99
+        else:
+            return self.number - 1
+
 
 if __name__ == '__main__':
     bottle = Bottles()
