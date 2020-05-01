e59aed40c29ad117cab0e95ad23827c7420a81db
Sue Liu <sue.liu@mudano.com>
2020-05-01 20:42:33 +0100

    Add house code

---
+++ pyhouse/lib/house.py
@@ -0,0 +1,6 @@
+class House:
+    def __init__(self):
+        pass
+
+    def line(self, number):
+        pass
---
+++ pyhouse/test/house_test.py
@@ -0,0 +1,59 @@
+import sys
+sys.path.append('.')
+import unittest
+import pytest
+from pyhouse.lib.house import House
+
+
+class HouseTest(unittest.TestCase):
+    def setUp(self):
+        self.tale = House()
+
+    def test_line_1(self):
+        expected = "This is the house that Jack built.\n"
+        self.assertEqual(expected, self.tale.line(1))
+
+    def test_line_2(self):
+        expected = "This is the malt that lay in the house that Jack built.\n"
+        self.assertEqual(expected, self.tale.line(2))
+
+    def test_line_3(self):
+        expected = "This is the rat that ate the malt that lay in the house that Jack built.\n"
+        self.assertEqual(expected, self.tale.line(3))
+
+    def test_line_4(self):
+        expected = "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        self.assertEqual(expected, self.tale.line(4))
+    
+    def test_line_5(self):
+        expected = "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        self.assertEqual(expected, self.tale.line(5))
+
+    def test_line_6(self):
+        expected = "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        self.assertEqual(expected, self.tale.line(6))
+
+    def test_line_7(self):
+        expected = "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        self.assertEqual(expected, self.tale.line(7))
+
+    def test_line_8(self):
+        expected = "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        self.assertEqual(expected, self.tale.line(8))
+  
+    def test_line_9(self):
+        expected = "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        self.assertEqual(expected, self.tale.line(9))
+  
+    def test_line_10(self):
+        expected = "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        self.assertEqual(expected, self.tale.line(10))
+  
+    def test_line_11(self):  
+        expected = "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        self.assertEqual(expected, self.tale.line(11))
+  
+
+    def test_line_12(self):
+        expected = "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        self.assertEqual(expected, self.tale.line(12))
