fcbfd108f5ce73bbbeb79ea7216aab8166b61316
yusueliu <sliu@usertesting.com>
2020-05-01 21:47:50 +0100

    house_shameless

--- pyhouse/lib/house.py
+++ pyhouse/lib/house.py
@@ -1,6 +1,20 @@
 class House:
-    def __init__(self):
-        pass
 
     def line(self, number):
-        pass
+        switcher = {1: "This is the house that Jack built.\n",
+        2: "This is the malt that lay in the house that Jack built.\n",
+        3:  "This is the rat that ate the malt that lay in the house that Jack built.\n",
+        4: "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+        5: "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+        6: "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+        7: "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+        8: "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+        9: "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+        10: "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+        11: "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+        12:"This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        }
+        return switcher[number]
+
+
+    
--- pyhouse/test/test_house.py
+++ pyhouse/test/test_house.py
@@ -1,9 +1,6 @@
-import sys
-sys.path.append('.')
 import unittest
 import pytest
 from pyhouse.lib.house import House
-
 
 class HouseTest(unittest.TestCase):
     def setUp(self):
@@ -53,7 +50,9 @@ class HouseTest(unittest.TestCase):
         expected = "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         self.assertEqual(expected, self.tale.line(11))
   
-
     def test_line_12(self):
         expected = "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         self.assertEqual(expected, self.tale.line(12))
+
+if __name__ == '__main__':
+    unittest.main()
