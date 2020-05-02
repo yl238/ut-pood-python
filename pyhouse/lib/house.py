69d9e0828c2fd643e102fbad957e06c57461f863
yusueliu <sliu@usertesting.com>
2020-05-02 10:40:22 +0100

    Worked all the way to random column shuffling

--- pyhouse/lib/house.py
+++ pyhouse/lib/house.py
@@ -1,28 +1,5 @@
 import random
 
-class RandomOrderer(object):
-    def order(self, data):
-        random.shuffle(data)
-        return data
-    
-class OriginalOrderer(object):
-    def order(self, data):
-        return data
-
-class RandomButLastOrderer(object):
-    def order(self, data):
-        random.shuffle(data[:-1])
-        data.append(data[-1])
-        return data
-
-class PiratePrefixer(object):
-    def prefix(self):
-        return 'Thar be'
-
-class MundanePrefixer(object):
-    def prefix(self):
-        return 'This is'
-
 HOUSE_DATA = [
     ["the horse and the hound and the horn", "", "that belonged to"],
     ["the farmer", "sowing his corn", "that kept"],
@@ -39,12 +16,46 @@ HOUSE_DATA = [
 ]
 
 
+class RandomOrderer(object):
+    def order(self, data):
+        random.shuffle(data)
+        return data
+    
+
+class OriginalOrderer(object):
+    def order(self, data):
+        return data
+
+
+class RandomButLastOrderer(object):
+    def order(self, data):
+        random.shuffle(data[:-1])
+        return data
+
+
+class MixedColumnOrderer(object):
+    def order(self, data):
+        for el in data: random.shuffle(el)
+        return data
+
+
+class PiratePrefixer(object):
+    def prefix(self):
+        return 'Thar be'
+
+
+class MundanePrefixer(object):
+    def prefix(self):
+        return 'This is'
+
+
 class Phrases(object):
     def __init__(self, orderer=OriginalOrderer(), input_data=HOUSE_DATA):
         self.data = orderer.order(input_data)
 
     def phrase(self, num):
-        return ' '.join([item for sublist in self.data[-num:] for item in sublist])
+        flatten = [item for sublist in self.data[-num:] for item in sublist]
+        return ' '.join(' '.join(flatten).split())
         
     def size(self):
         return len(self.data)
@@ -53,17 +64,17 @@ class Phrases(object):
 class CumulativeTale(object):
     def __init__(self, phrases=Phrases(), prefixer=MundanePrefixer()):
         self.phrases = phrases
-        self.prefix = prefixer
+        self.prefix = prefixer.prefix()
 
     def recite(self):
         return '\n'.join([self.line(i) for i in range(1, 13)])
 
     def phrase(self, number):
-        self.phrases.phrase(number)
+        return self.phrases.phrase(number)
 
     def line(self, number):
         return f"{self.prefix} {self.phrase(number)}.\n"
 
 
 if __name__ == '__main__':
-    print(CumulativeTale().recite)
+    print(CumulativeTale(phrases=Phrases(orderer=MixedColumnOrderer())).line(12))
