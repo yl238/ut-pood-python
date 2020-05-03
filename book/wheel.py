d5d0698c559c81aad1224619c15b909997e8521e
Sue Liu <39272585+yusueliu@users.noreply.github.com>
2020-05-03 19:25:03 +0100

    change to keyword arguments

--- book/wheel.py
+++ book/wheel.py
@@ -2,27 +2,31 @@ class Wheel(object):
     def __init__(self, rim, tire):
         self.rim = rim
         self.tire = tire
+        self.diameter = self.get_diameter()
 
-    def diameter(self):
+    def get_diameter(self):
         return self.rim + (self.tire * 2)
 
 
 class Gear(object):
-    def __init__(self, chainring, cog, rim, tire):
-        self.chainring = chainring
+    def __init__(self, chainring=None, cog=None, wheel=None):
+        self.chainring = chainring or self.default_chainring()
         self.cog = cog
-        self.rim = rim
-        self.tire= tire
+        self.wheel = wheel
 
     def ratio(self):
         return self.chainring / float(self.cog)
 
     def gear_inches(self):
-        return self.ratio() * Wheel(self.rim, self.tire).diameter()
+        return self.ratio() * self.diameter()
 
+    def diameter(self):
+        return self.wheel.diameter
+
+    def default_chainring(self):
+        return (100/2) - 10
 
 if __name__ == '__main__':
-    print(Gear(52,11, 26, 1.5).gear_inches())
-
+    print(Gear(cog=11, wheel=Wheel(26, 1.5)).gear_inches())
 
 
